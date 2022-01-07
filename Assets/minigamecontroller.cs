a28b5b79e227e0d39689d9093cc8ca3841de1a09
flying walrus <eveliina.schuurman@metropolia.fi>
2022-01-07 21:03:29 +0200

    first minigame is playable!

---
+++ Assets/minigamecontroller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class minigamecontroller : MonoBehaviour
+{
+    //find_and_click
+    public Button find1;
+    public Button find2; 
+    public Button find3;
+    private int objs_found = 0;
+    private int objs_needed = 3;
+
+    public Text youWonText;
+
+    public void Start()
+    {
+        find1.gameObject.SetActive(true);
+        find2.gameObject.SetActive(true); 
+        find3.gameObject.SetActive(true);
+        find1.gameObject.GetComponent<Button>().onClick.AddListener(delegate{objClick(find1);});
+        find2.gameObject.GetComponent<Button>().onClick.AddListener(delegate { objClick(find2); });
+        find3.gameObject.GetComponent<Button>().onClick.AddListener(delegate { objClick(find3); });
+
+
+    }
+
+    public void objClick(Button s)
+    {
+        s.gameObject.SetActive(false);
+        objs_found++;
+        if(objs_found == objs_needed)
+        {
+            youWonText.text = "You won! Here's an item for you";
+            Debug.Log("you won!");
+        }
+    }
+
+}
