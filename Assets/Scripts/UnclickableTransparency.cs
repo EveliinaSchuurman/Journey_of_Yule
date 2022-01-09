a9b8b1331b54fffc378d26743b7831aa70d40ad1
johannmi <49305031+johannmi@users.noreply.github.com>
2022-01-09 19:01:37 +0200

    Battle UI

---
+++ Assets/Scripts/UnclickableTransparency.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnclickableTransparency : MonoBehaviour
+{
+
+    void Start()
+    {
+        gameObject.GetComponent<Image>().alphaHitTestMinimumThreshold = 1.0f;
+    }
+}
