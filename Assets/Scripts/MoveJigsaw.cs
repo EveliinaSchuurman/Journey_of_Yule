a0bfc151d1d78d012430119cfc56bb40ea71b794
flying walrus <eveliina.schuurman@metropolia.fi>
2022-01-10 21:51:33 +0200

    minigame 3 ready

---
+++ Assets/Scripts/MoveJigsaw.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class MoveJigsaw : MonoBehaviour
+{
+    private Vector3 rightPos;
+    public bool inRightPos;
+    public bool Selected;
+    public bool counted = false;
+    private Jigsaw ji;
+    void Start()
+    {
+        ji = FindObjectOfType<Jigsaw>();
+        rightPos = transform.position;
+        transform.position = new Vector3(Random.Range(-1.91f, -1f), Random.Range(3.79f, 3f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(rightPos, transform.position) < 0.4f)
+        {
+            if (!Selected)
+            {
+                if(inRightPos == false)
+                {
+                    transform.position = rightPos;
+                    inRightPos = true;
+                    ji.AddRights();
+                    GetComponent<SortingGroup>().sortingOrder = -1;
+                }
+            }
+            
+        }
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -6,8 +6,7 @@ public class PlayerScript : MonoBehaviour
 {
     private int keys = 1;
     private int currentRoomNumber = 0;
-    private int roomsOpeneduntil = 0;
-    private bool justWonInTraining;
+    private int _openRooms = 0;
 
     private void Awake()
     {
@@ -38,12 +37,13 @@ public class PlayerScript : MonoBehaviour
     }
     public void OpenRoom()
     {
-        roomsOpeneduntil++;
+        _openRooms++;
     }
 
     public int GetOpenRooms()
     {
-        return roomsOpeneduntil;
+        //this can be an int because all doors can only be opened in order
+        return _openRooms;
     }
 
 }
