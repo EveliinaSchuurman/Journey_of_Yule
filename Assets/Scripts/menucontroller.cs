ad8894da32c0fff196ae8aaff9eb80f32f649bed
flying walrus <eveliina.schuurman@metropolia.fi>
2022-01-27 16:00:21 +0200

    added ad functionality. shows test ads when requested

---
+++ Assets/Scripts/menucontroller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menucontroller : MonoBehaviour
+{
+    public void Ad()
+    {
+        //ads.instance.RequestAndLoadRewardedAd();
+        if (ads.instance == null)
+            Debug.Log("no instance");
+        ads.instance.ShowRewardedAd();
+    }
+    public void ReqAd()
+    {
+        if (ads.instance == null)
+            Debug.Log("no instance");
+        ads.instance.RequestAndLoadRewardedAd();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
